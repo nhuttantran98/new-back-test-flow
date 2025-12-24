604bd7ca379397e36d7d947301090f748a46f9a9
tan.tran2@datalogic.com <D@talogic24>
2025-12-24 11:57:10 +0700

    push

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const multer = require('multer');
-const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
 const runTestCase = require('./utils/runTestCase').runTestCase;
-
+const convertCSVToJson = require('./utils/convertCSVToJson').convertCSVToJson;
 const app = express();
 
 const UPLOAD_DIR = path.join(__dirname, "uploads");
@@ -37,40 +36,7 @@ app.post('/run-test-case', (req, res) => {
 
 app.post("/convert", upload.single("file"), async (req, res) => {
     try {
-        const fileName = req.file.originalname;
-        const filePath = path.join(UPLOAD_DIR, fileName);
-        // --- 1. Read file Excel ---
-        const workbook = readFile(filePath);
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-
-        const rows = utils.sheet_to_json(worksheet, { defval: "" });
-
-        // --- 2. Group according to Test suite ---
-        const grouped = {};
-
-        rows.forEach((row) => {
-            const suiteName = row["Test Suite Execution Records"] || "Unknown Suite";
-
-            if (!grouped[suiteName]) {
-                grouped[suiteName] = { "Test suite name": suiteName };
-            }
-
-            const caseIndex = Object.keys(grouped[suiteName]).filter(k => k.startsWith("Test case")).length + 1;
-            const caseName = `Test case ${caseIndex}`;
-
-            grouped[suiteName][caseName] = row;
-        });
-
-        // --- 3. Write to out.json ---
-        const outJSONPath = path.join(OUTPUT_DIR, "out.json");
-        fs.writeFileSync(outJSONPath, JSON.stringify(grouped, null, 2), "utf8");
-
-        res.json({
-            success: true,
-            output: "/outputs/out.json"
-        });
-
+        convertCSVToJson(req.file, res);
     } catch (err) {
         console.error("Error converting:", err);
         res.status(500).json({ success: false, message: err.message });
